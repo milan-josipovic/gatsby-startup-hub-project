ef2fc720498e41ddf1438d764a546999250258e0
milan-josipovic <milan.josipovic@protonmail.com>
2020-06-12 22:32:53 +0200

    initia

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Layout from "../components/Layout"
+import Slide from "../components/Slide"
+export default () => (
+  <Layout>
+    Početna
+    <Slide />
+  </Layout>
+)
